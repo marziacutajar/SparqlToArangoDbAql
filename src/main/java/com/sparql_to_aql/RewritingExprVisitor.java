df516cf2b95033a27235500845dca8b664f106d5
Marzia Cutajar <marziacutajar@gmail.com>
2019-09-06 16:13:55 +0200

    Implemented expr rewriter

--- src/main/java/com/sparql_to_aql/RewritingExprVisitor.java
+++ src/main/java/com/sparql_to_aql/RewritingExprVisitor.java
@@ -3,12 +3,16 @@ package com.sparql_to_aql;
 import com.aql.algebra.expressions.constants.Const_Bool;
 import com.aql.algebra.expressions.constants.Const_Number;
 import com.aql.algebra.expressions.constants.Const_String;
-import com.aql.algebra.expressions.functions.Expr_Equals;
-import com.aql.algebra.expressions.functions.Expr_LogicalNot;
+import com.aql.algebra.expressions.functions.*;
 import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.expr.*;
-
+import org.apache.jena.sparql.expr.ExprFunction0;
+import org.apache.jena.sparql.expr.ExprFunction1;
+import org.apache.jena.sparql.expr.ExprFunction2;
+import org.apache.jena.sparql.expr.ExprFunction3;
+import org.apache.jena.sparql.expr.ExprFunctionN;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 //TODO IMP consider removing data type for literals from our arangodb documents - you'll know the type if the value is a number, a string, or a boolean, and if it's a language string it will have lang attribute
@@ -24,8 +28,16 @@ public class RewritingExprVisitor extends ExprVisitorBase {
 
     private LinkedList<com.aql.algebra.expressions.Expr> createdAqlExprs = new LinkedList<>();
 
+    private List<com.aql.algebra.expressions.Expr> GetCreatedAqlExprsForExpr(int numOfNestedExprs){
+        int from = createdAqlExprs.size() - numOfNestedExprs;
+        int to = createdAqlExprs.size() - 1;
+        List<com.aql.algebra.expressions.Expr> exprsToReturn = createdAqlExprs.subList(from, to);
+        createdAqlExprs.removeAll(exprsToReturn);
+        return exprsToReturn;
+    }
+
     public com.aql.algebra.expressions.Expr getFinalAqlExpr(){
-        return createdAqlExprs.getFirst();
+        return createdAqlExprs.get(0);
     }
 
     public RewritingExprVisitor(Map<String, String> boundVariables){
@@ -45,44 +57,51 @@ public class RewritingExprVisitor extends ExprVisitorBase {
         //not a priority but possibly support ABS, CEILING, FLOOR, ROUND, STR_LOWER, STR_UPPER, STR_LENGTH
         // remember that for this we'd have to check the VALUE of the arangodb document
         if(func instanceof E_LogicalNot){
-            //TODO
+            createdAqlExprs.push(new Expr_LogicalNot(createdAqlExprs.removeLast()));
         }
         else{
             throw new UnsupportedOperationException("SPARQL expression not supported!");
         }
     }
 
-    //TODO implement below + consider improving using enum + switch
+    //TODO consider improving using enum + switch
     @Override
     public void visit(ExprFunction2 func){
+        com.aql.algebra.expressions.Expr param1 = createdAqlExprs.removeLast();
+        com.aql.algebra.expressions.Expr param2 = createdAqlExprs.removeLast();
+
+        com.aql.algebra.expressions.Expr aqlExpr;
+
         if(func instanceof E_Add){
-
+            aqlExpr = new Expr_Add(param1, param2);
         }else if(func instanceof E_Divide){
-
+            aqlExpr = new Expr_Divide(param1, param2);
         }else if(func instanceof E_Equals){
-
+            aqlExpr = new Expr_Equals(param1, param2);
         }else if(func instanceof E_GreaterThan){
-
+            aqlExpr = new Expr_GreaterThan(param1, param2);
         }else if(func instanceof E_GreaterThanOrEqual){
-
+            aqlExpr = new Expr_GreaterThanOrEqual(param1, param2);
         }else if(func instanceof E_LessThan){
-
+            aqlExpr = new Expr_LessThan(param1, param2);
         }else if(func instanceof E_LessThanOrEqual){
-
+            aqlExpr = new Expr_LessThanOrEqual(param1, param2);
         }else if(func instanceof E_LogicalAnd){
-
+            aqlExpr = new Expr_LogicalAnd(param1, param2);
         }else if(func instanceof E_LogicalOr){
-
+            aqlExpr = new Expr_LogicalOr(param1, param2);
         }else if(func instanceof E_Multiply){
-
+            aqlExpr = new Expr_Multiply(param1, param2);
         }else if(func instanceof E_NotEquals){
-
+            aqlExpr = new Expr_NotEquals(param1, param2);
         }else if(func instanceof E_Subtract){
-
+            aqlExpr = new Expr_Subtract(param1, param2);
         }
         else{
             throw new UnsupportedOperationException("SPARQL expression not supported!");
         }
+
+        createdAqlExprs.push(aqlExpr);
     }
 
     //encountered in case like (x == y) ? opt1 : opt2
@@ -105,24 +124,27 @@ public class RewritingExprVisitor extends ExprVisitorBase {
     @Override
     public void visit(NodeValue nv){
         //handle different types of values - we're only gonna allow "constants"
+        com.aql.algebra.expressions.Expr aqlExpr;
+
         if(nv.isBoolean()){
-            new Const_Bool(nv.getBoolean());
+            aqlExpr = new Const_Bool(nv.getBoolean());
         }
         else if(nv.isString()) {
-            new Const_String(nv.getString());
+            aqlExpr = new Const_String(nv.getString());
         }
         else if(nv.isNumber()){
-            new Const_Number(nv.getDouble());
+            aqlExpr = new Const_Number(nv.getDouble());
         }
         else{
             throw new UnsupportedOperationException("Node value in SPARQL expression not supported!");
         }
+
+        createdAqlExprs.push(aqlExpr);
     }
 
     @Override
-    public void visit(ExprVar nv){
-        //TODO
-        nv.getVarName();
+    public void visit(ExprVar e){
+        createdAqlExprs.push(new com.aql.algebra.expressions.ExprVar(e.getVarName()));
     }
 
     @Override
